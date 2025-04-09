88a438a217b4985d2eb7328b3f982c177658d3cf
pedro <pedro092692@gmail.com>
2025-04-09 12:38:20 +0200

    add metadata

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
 export const experimental_ppr = true;
+import { Metadata } from 'next';
+
 
 export default function Layout({children}: {children: React.ReactNode}){
     return (
